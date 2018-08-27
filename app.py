285013e337e5517844c974fcdf3d768b29fd7ca4
pavin <pavithranthilakanathan@gmail.com>
2018-08-27 17:57:14 +0530

    document download options

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template, request, json, jsonify
+from flask import Flask, render_template, request, json, jsonify, send_file
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 import cv2
 import os
 import logging
+import time
+import datetime
+from reportlab.pdfgen import canvas
 from logging.handlers import RotatingFileHandler
+from docx import Document
+from docx.shared import Inches
 
 from ocr.normalization import imageNorm, letterNorm
 from ocr import page, words, charSeg
 from ocr.helpers import implt, resize
 from ocr.tfhelpers import Graph
 from ocr.datahelpers import idx2char
+
+current_milli_time = lambda: int(round(time.time() * 1000))
 
 import Cycler
 
@@ -35,6 +42,7 @@ def main():
 @app.route("/recognize", methods=['POST'])
 def recognize():
     if request.method == 'POST':
+        # start = current_milli_time()
         if 'image' not in request.files:
             print('No file part')
             return render_template('index.html')
@@ -56,9 +64,40 @@ def recognize():
             for i in range(len(bBoxes)-1):
                 allWords.append(cycler.idxImage(i))
                 print(allWords[i])
-
+    # stop = current_milli_time()
+    # print(stop - start)
 
     return jsonify(allWords=allWords)
 
+@app.route("/savePdf", methods=['POST'])
+def savePdf():
+    if request.method == 'POST':
+        text = request.form['text']
+        t = datetime.datetime.now()
+        current = t.strftime('%m-%d-%y')
+        save_name = "doc/pdf_" + current + ".pdf"
+        c = canvas.Canvas(save_name)
+        c.drawString(40, 800, text)
+        c.save()
+        return send_file(save_name, as_attachment=True)
+
+@app.route("/saveDoc", methods=['POST'])
+def saveDoc():
+    if request.method == 'POST':
+        text = request.form['textDoc']
+        # print("text", text)
+        t = datetime.datetime.now()
+        current = t.strftime('%m-%d-%y')
+        save_name = "doc/docx_" + current + ".docx"
+        document = Document()
+        document.add_heading('Recognized text', 0)
+
+        p = document.add_paragraph(text)
+        document.add_page_break()
+        document.save(save_name)
+        return send_file(save_name, as_attachment=True)
+
 if __name__ == "__main__":
-    app.run()
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True)
