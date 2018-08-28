1cc2d0e9d0acb33146d4100d686f8c28fea5495a
pavin <pavithranthilakanathan@gmail.com>
2018-08-28 11:01:10 +0530

    Fixed image path

--- tests/test_graynoalpha.py
+++ tests/test_graynoalpha.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 def test_graynoalpha():
-    img = cv2.cvtColor(cv2.imread("data/pagedet/1.jpg"), cv2.COLOR_BGR2RGB)
+    img = cv2.cvtColor(cv2.imread("images/test1.jpg"), cv2.COLOR_BGR2RGB)
     W = [0.11,0.59,0.3] # weights
     W_mean = np.tensordot(img,W, axes=((-1,-1)))[...,None]
     img[:] = W_mean.astype(img.dtype)
